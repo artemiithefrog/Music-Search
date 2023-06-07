d29523ea2f0d906f3f3c9b001486d8ef16ebfc6f
artemiithefrog <115695294+artemiithefrog@users.noreply.github.com>
2023-06-07 12:38:34 +0200

    Initial Commit

---
+++ Music Search/Music_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_SearchApp.swift
+//  Music Search
+//
+//  Created by artemiithefrog on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Music_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
